43a521df3202936bb8cce1d5b72bbe3b9458c3b9
Arafat420 <arafatpstu7@gmail.com>
2025-04-14 18:44:47 +0600

    set practice

---
+++ duplicate.cpp
@@ -0,0 +1,79 @@
+/*
+set problem
+problem link:https://leetcode.com/problems/contains-duplicate/description/
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define nl '\n'
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+
+#define show(x) cout << #x << " : " << x << endl
+
+void solve(int tc)
+{
+    int n;
+    cin >> n;
+    set<int> st;
+    for (int i = 0; i < n;i++)
+    {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    if(st.size()<n)
+    {
+        cout << "true" << nl;
+    }
+    else
+    {
+        cout << "false" << nl;
+    }
+}
+
+int main()
+{
+ ios_base::sync_with_stdio(0);
+  cin.tie(0);
+    int tc = 1;
+    //cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+}
